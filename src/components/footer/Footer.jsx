546e4f3da7a18b55a9265eea763d0e76fa956ed5
hugooSoul <mandujano@Hugos-MacBook-Pro.local>
2024-04-02 14:01:15 -0600

    adding styles to some components like Links, Navbar, and using Image component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Hugo Test</div>
+      <div className={styles.text}>
+        Hugo creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
