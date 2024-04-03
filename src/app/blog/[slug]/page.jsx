ce341819082a13a6980ec30fae0c19c5ceca3920
hugooSoul <mandujano@Hugos-MacBook-Pro.local>
2024-04-03 12:27:30 -0600

    fetching data from API and without API

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,21 +1,47 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
+import { getPost } from "@/lib/data";
 
-const Post = async () => {
+// FETCH DATA WITH AN API
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+export const generateMetadata = async ({ params }) => {
+  const { slug } = params;
+
+  const post = await getPost(slug);
+
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+};
+
+const Post = async ({ params }) => {
   const { slug } = params;
 
   // FETCH DATA WITH AN API
-  const post = await getData(slug);
+  // const post = await getData(slug);
+  const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
-      {post.img && (
+      {!post.img && (
         <div className={styles.imgContainer}>
           <Image src={post.img} alt="" fill className={styles.img} />
         </div>
       )}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post.title}</h1>
+        <h1 className={styles.title}>{post?.title}</h1>
         <div className={styles.detail}>
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
