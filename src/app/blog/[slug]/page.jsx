c2799086d9dff8a0d1b25b289c8355b63fa383f0
hugooSoul <mandujano@Hugos-MacBook-Pro.local>
2024-04-03 08:45:31 -0600

    Adding styles to views

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,38 @@
-const Post = () => {
-  return <div>Post  Page</div>;
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
+const Post = async () => {
+  const { slug } = params;
+
+  // FETCH DATA WITH AN API
+  const post = await getData(slug);
+
+  return (
+    <div className={styles.container}>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.desc}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
